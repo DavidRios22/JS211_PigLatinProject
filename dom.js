aaa765c82aca21d949829b58335ff33a57f44a68
DavidRios22 <davidriosdoeswork@gmail.com>
2023-02-17 16:36:39 -0600

    added gui

---
+++ dom.js
@@ -0,0 +1,19 @@
+
+const pigLatin = () => {
+
+  let word = document.getElementById("user-input").value
+
+  word = word.trim().toLowerCase()
+
+  let vowels = ['a', 'e', 'i', 'o', 'u']
+
+  for (let letter = 0; letter < word.length; letter++) {
+    if (vowels.includes(word[0])) {
+      return document.getElementById("display-element").innerHTML = word + "yay"
+    } else if ((!vowels.includes(word[0])) && (!vowels.includes(word[1]))) {
+      return document.getElementById("display-element").innerHTML = word.slice(2) + word.slice(0, 2) + "ay"
+    } else {
+      return document.getElementById("display-element").innerHTML = word.slice(1) + word.slice(0, 1) + "ay"
+    }
+  }
+}
